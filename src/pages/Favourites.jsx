1d4c34ce1e6889bbbe03bc6c51b0adc5b1b88afa
ANURAG36IN6BALLS <98388444+ANURAG36IN6BALLS@users.noreply.github.com>
2023-04-20 16:03:06 +0530

    something not responsive in my cart n favorite

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function Favourites() {
   return (
-    <>
-      <h1 className="text-center text-2xl font-bold mt-10 ">My Wish List</h1>
-      <div className="m-20 flex space-x-20">
-        <div className="w-[30%] border-2 border-black  font-semibold mt-10 text-sm">
+    <div className="w-full my-10">
+      <h1 className="text-center text-2xl font-bold  ">My Wish List</h1>
+      <div className="lg:m-20 flex lg:space-x-20">
+        <div className="w-[30%] hidden lg:block border-2 border-black  font-semibold mt-10 text-sm">
           <p className="hover:text-white hover:bg-black border-b-2 p-4 border-black">
             My Account
           </p>
@@ -36,15 +36,19 @@ export default function Favourites() {
           <p className="hover:text-white hover:bg-black  p-4 ">Logout</p>
         </div>
 
-        <div className="w-full">
-          <div className="flex space-x-40 border-2 border-gray-300 mt-10 w-[90%] px-5n shadow-md h-fit justify-around">
-            <div>
-              <h1 className="font-bold font-serif my-6 text-2xl">
+        <div className="w-full px-4">
+          <div className="lg:flex lg:pl-[20px] border-2 border-gray-300 mt-10 w-[90%]  shadow-md h-fit justify-around">
+            <div className="w-fit m-auto">
+              <h1 className="font-bold font-serif my-6 xm:text-sm lg:text-2xl">
                 Account Information
               </h1>
-              <p className="my-4 text-xl font-bold">Contact Information</p>
-              <p className="text-md">Deeksha Bhargava</p>
-              <p className="text-md mb-6">1886693195010923@facebook.com</p>
+              <p className="my-4 xm:text-sm lg:text-xl font-bold">
+                Contact Information
+              </p>
+              <p className="xm:text-sm lg:text-md">Deeksha Bhargava</p>
+              <p className="xm:text-sm lg:text-md  mb-6 ">
+                1886693195010923@facebook.com
+              </p>
 
               <button className="p-2 w-14 rounded bg-yellow-400 mr-2 my-4">
                 Edit
@@ -53,22 +57,28 @@ export default function Favourites() {
                 Change Password
               </button>
             </div>
-            <div className="py-16">
-              <h1 className="my-4 text-xl font-bold">Newsletters</h1>
+            <div className="py-16 w-[80%] m-auto">
+              <h1 className="my-4 xm:text-sm lg:text-xl font-bold">
+                Newsletters
+              </h1>
               <p className="mb-4">You aren't subscribed to our newsletter.</p>
               <button className="p-2 w-14 rounded bg-yellow-400 mr-2 ">
                 Edit
               </button>
             </div>
           </div>
-          <div className="flex space-x-40 border-2 border-gray-300 mt-10 w-[90%] px-5n  shadow-md h-fit justify-around">
-            <div>
-              <h1 className="font-bold font-serif my-6 text-2xl">
+          <div className="lg:flex lg:pl-[20px] border-2 border-gray-300 mt-10 w-[90%]  shadow-md h-fit justify-around">
+            <div className="w-fit m-auto">
+              <h1 className="font-bold font-serif my-6 xm:text-sm lg:text-2xl">
                 Account Information
               </h1>
-              <p className="my-4 text-xl font-bold">Contact Information</p>
-              <p className="text-md">Deeksha Bhargava</p>
-              <p className="text-md mb-6">1886693195010923@facebook.com</p>
+              <p className="my-4 xm:text-sm lg:text-xl font-bold">
+                Contact Information
+              </p>
+              <p className="xm:text-sm lg:text-md">Deeksha Bhargava</p>
+              <p className="xm:text-sm lg:text-md  mb-6 ">
+                1886693195010923@facebook.com
+              </p>
 
               <button className="p-2 w-14 rounded bg-yellow-400 mr-2 my-4">
                 Edit
@@ -77,8 +87,10 @@ export default function Favourites() {
                 Change Password
               </button>
             </div>
-            <div className="py-16">
-              <h1 className="my-4 text-xl font-bold">Newsletters</h1>
+            <div className="py-16 w-[80%] m-auto">
+              <h1 className="my-4 xm:text-sm lg:text-xl font-bold">
+                Newsletters
+              </h1>
               <p className="mb-4">You aren't subscribed to our newsletter.</p>
               <button className="p-2 w-14 rounded bg-yellow-400 mr-2 ">
                 Edit
@@ -87,6 +99,6 @@ export default function Favourites() {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
